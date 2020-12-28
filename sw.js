1fd6a133e1da4dc22c6cc71ad779449671b3ea1d
Piyush Ingale <68149489+PiyushIngale007@users.noreply.github.com>
2020-12-28 23:15:56 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var cacheName = 'hello-pwa'
+var cacheName = 'PWA_Calculator'
 var filesToCache = ['/', '/index.html', '/css/style.css', '/js/main.js']
 
 /* Start the service worker and cache all of the app's content */
